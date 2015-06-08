2ac92935a159a8c5ab07d982ec9c9104a0fa30d3
Ryan Kinal <ryan.kinal@gmail.com>
2015-06-08 16:32:25 -0400

    Added list rendering

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@
 
 (function() {
 	var borrowed = [],
-		addItem = function(who, what, when) {
+		list = document.getElementById('stuff');
+	
+	var addItem = function(who, what, when) {
 			borrowed.push({
 				who: who,
 				what: what,
@@ -10,7 +12,19 @@
 			});
 		};
 
+	var render = function() {
+		borrowed.forEach(function(item) {
+			var listItem = document.createElement('li'),
+				text = document.createTextNode(item.who + ' has my ' + item.what + '(' + item.when ')');
+
+			listItem.appendChild(text);
+			list.appendChild(text);
+		});
+	};
+
 	addItem('dan', 'the good parts', Date.now());
 
 	console.log(borrowed);
+
+	render();
 }());
